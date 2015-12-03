766c2e1ffad45f2e81c2f98ffc2a6835b45b2cbb
Dominic Bennett <dominic.john.bennett@gmail.com>
2015-12-03 13:02:25 +0000

    Removed print statements

--- R/misc.R
+++ R/misc.R
@@ -1,6 +1,5 @@
 randTree <- function (n) {
   .node <- function (n_left, id, span, prenode, predist, nodelist) {
-    print (n_left)
     predist <- predist + span
     postnode <- children <- c ()
     pd <- 0
@@ -19,8 +18,6 @@ randTree <- function (n) {
       n_left[1] <- abs (n_left[1] - n_left[2])
       for (n in n_left) {
         new_id <- paste0 ('n', length (nodelist) + 1)
-        print (length (nodelist))
-        print (new_id)
         postnode <- c (postnode, new_id)
         new_span <- runif (min=0, max=1, n=1)
         pd <- new_span + pd
@@ -43,5 +40,9 @@ randTree <- function (n) {
   id <- paste0 ('n', 1)
   predist <- span <- 0
   prenode <- c ()
+  # gen nodelist
   nodelist <- .node (n_left, id, span, prenode, predist, nodelist)
+  # init new tree object
+  tree <- new ('NodeList', nodelist=nodelist, root='n1')
+  .update (tree)
 }
