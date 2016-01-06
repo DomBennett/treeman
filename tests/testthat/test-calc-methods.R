c6d6fcfba0a54e6ec05febdcd797519bcceae9ed
Dominic Bennett <dominic.john.bennett@gmail.com>
2016-01-06 18:22:56 +0000

    Extra test for calcPhyDiv

--- tests/testthat/test-calc-methods.R
+++ tests/testthat/test-calc-methods.R
@@ -10,7 +10,15 @@ test_that('calcPhyDiv() works', {
   pd <- calcPhyDiv(tree, tips)
   parent <- getParent(tree, nodes=tips)
   test_that(pd, is_less_than(tree@nodelist[[parent]]$pd))
-  #TODO use addTip to add a tip with a specified length.
+  # add a tip with a specified length.
+  sister <- sample(tips, 1)
+  sister_age <- getNodeAge(tree, sister)
+  parent_age <- getNodeAge(tree, tree@nodelist[[sister]]$prenode)
+  start <- runif(min=sister_age, max=parent_age, n=1)
+  end <- runif(min=0, max=start, n=1)
+  tree <- addTip(tree, id='new_tip', sister=sister, start=start, end=end)
+  new_pd <- calcPhyDiv(tree, c(tips, 'new_tip'))
+  test_that(new_pd, equals(pd + (start - end)))
 })
 test_that('calcFairProp() works', {
   #TODO
