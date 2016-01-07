b83de60ecdc27627f6b2cd2b1483fd63459915f5
Dominic Bennett <dominic.john.bennett@gmail.com>
2016-01-07 18:20:10 +0000

    Notes on how to resolve error
    — pin names is failing because age of tree is sometimes increasing when
    a new tip is added

--- other/development.R
+++ other/development.R
@@ -1,7 +1,10 @@
 # TODO:
 # -- read/write newick
-# -- get functions (esp. parent, postnodes)
 
-library (treeman)
-tree <- randTree (10)
-viz (tree)
+# TODO -- why does this lead to an increase in age sometimes?
+test_tree <- randTree(10)
+rng <- getEdgeAge(test_tree, 'n8')
+start <- runif(max=rng$max, min=rng$min, n=1)
+end <- runif(max=start, min=0, n=1)
+test_tree2 <- addTip(test_tree, id='new', sister='n8', end=end, start=start)
+test_tree2@age == test_tree@age
