6b538c571faf0bf3c10a62d75354e5720f1c71ed
Dominic Bennett <dominic.john.bennett@gmail.com>
2016-01-07 18:19:04 +0000

    Created pin methods

--- R/manip-methods.R
+++ R/manip-methods.R
@@ -1,13 +1,14 @@
-# TODO: removeTip, mergeTree, collapseNode
-
-addTip <- function(tree, id, sister, start, end) {
+# TODO: addTips, removeTip, mergeTree, collapseNode
+# TODO: add doc for adding and removing tips
+addTip <- function(tree, id, sister, start, end,
+                   parent_id=paste0("p_", id)) {
   updatePrenodes <- function(node) {
     node$children <- c(node$children, tip$id)
     node$pd <- node$pd + tip$span
     node
   }
   tip <- list('id'=id)
-  node <- list('id'=paste0("p", id))
+  node <- list('id'=parent_id)
   tip$span <- start - end
   age <- getNodeAge(tree, sister)
   new_sister <- sister <- tree@nodelist[[sister]]
@@ -32,4 +33,30 @@ addTip <- function(tree, id, sister, start, end) {
   tree@nodelist[prenodes] <- lapply(tree@nodelist[prenodes],
                                     updatePrenodes)
   .update(tree)
+}
+#TODO: add doc on pinning tips
+pinTip <- function(tree, tip_id, lineage, end) {
+  lineage <- lineage[lineage != tree@root]
+  rngs <- getEdgesAge(tree, lineage)
+  print("----")
+  print(end)
+  print(rngs)
+  print("----")
+  rngs <- rngs[rngs[ ,'max'] > end, ]
+  rngs[rngs[ ,'min'] <= end, "min"] <- end
+  prbs <- rngs$max - rngs$min
+  nd <- as.vector(sample(rngs$edge, prob=prbs, size=1))
+  i <- which(rngs$edge == nd)
+  start <- runif(min=rngs$min[i], max=rngs$max[i], n=1)
+  tree <- addTip(tree, id=tip_id, sister=nd, start=start, end=end)
+  tree
+}
+
+pinTips <- function(tree, tip_ids, lineages, ends) {
+  .pin <- function(i) {
+    tree <- pinTip(tree, tip_ids[i], lineages[[i]], ends[i])
+    tree <<- tree
+  }
+  sapply(1:length(tip_ids), .pin)
+  tree
 }
