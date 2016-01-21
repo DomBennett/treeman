924d48a1a669026479f08f38579f486717edc4ea
Dominic Bennett <dominic.john.bennett@gmail.com>
2016-01-21 19:11:33 +0000

    Minor style changes

--- R/display-methods.R
+++ R/display-methods.R
@@ -1,27 +1,27 @@
 # TreeMan display methods
 
-setMethod ('as.character', c('x'='TreeMan'),
+setMethod('as.character', c('x'='TreeMan'),
            function(x) {
-             paste0 ('TreeMan Object of [', length (x@tips),'] tips')
+             paste0('TreeMan Object of [', length(x@tips),'] tips')
            })
-setMethod ('show', 'TreeMan',
+setMethod('show', 'TreeMan',
            function(object){
-             print (object)
+             print(object)
            })
-setMethod ('str', c('object'='TreeMan'),
-           function (object, max.level=2L, ...) {
-             if (is.na (max.level)) {
-               stop ('max.level must be numeric')
+setMethod('str', c('object'='TreeMan'),
+           function(object, max.level=2L, ...) {
+             if(is.na(max.level)) {
+               stop('max.level must be numeric')
              }
-             str@default (object, max.level=max.level, ...)
+             str@default(object, max.level=max.level, ...)
            })
-setGeneric ('print')
-setMethod ('print', c('x'='TreeMan'),
+setGeneric('print')
+setMethod('print', c('x'='TreeMan'),
            function(x){
              msg <- 'Tree (TreeMan Object):\n'
              msg <- paste0(msg, '  + ', x@ntips, ' tips\n')
              msg <- paste0(msg, '  + ', x@nnodes, ' internal nodes\n')
-             if (x@ply) {
+             if(x@ply) {
                msg <- paste0(msg, '  + Polytomous\n')
              } else {
                msg <- paste0(msg, '  + Binary\n')
@@ -49,26 +49,26 @@ setMethod ('print', c('x'='TreeMan'),
              }
              cat(msg)
            })
-setGeneric ("viz", signature=c("tree", "taxonyms"),
-            function (tree, taxonyms=FALSE) {
+setGeneric("viz", signature=c("tree", "taxonyms"),
+            function(tree, taxonyms=FALSE) {
               standardGeneric("viz")
             })
-setMethod ('viz', 'TreeMan',
+setMethod('viz', 'TreeMan',
            function(tree, taxonyms){
-             get_pnts <- function (node, y, pnts) {
+             get_pnts <- function(node, y, pnts) {
                pstids <- node[['ptid']]
                low_y_diff <- -node[['pd']]/2
                high_y_diff <- node[['pd']]/2
                y_diffs <- seq(from=low_y_diff, to=high_y_diff,
                               length.out=length(pstids))
                counter <- 1
-               for (pstid in pstids) {
+               for(pstid in pstids) {
                  pstnd <- tree@nodelist[[pstid]]
                  pstnd_x <- pstnd[['prdst']]
                  pstnd_y <- y + y_diffs[counter]
-                 pnts <- rbind (pnts, data.frame (node=pstid,
+                 pnts <- rbind(pnts, data.frame(node=pstid,
                                                   x=pstnd_x, y=pstnd_y))
-                 pnts <- get_pnts (pstnd, pstnd_y, pnts)
+                 pnts <- get_pnts(pstnd, pstnd_y, pnts)
                  counter <- counter + 1
                }
                pnts
@@ -85,27 +85,27 @@ setMethod ('viz', 'TreeMan',
              }
              # start with root node
              # TODO: handle unrooted tree
-             pnts <- data.frame (node=tree@root, x=0, y=tree@pd, stringsAsFactors=FALSE)
+             pnts <- data.frame(node=tree@root, x=0, y=tree@pd, stringsAsFactors=FALSE)
              root_node <- tree@nodelist[[tree@root]]
-             pnts <- get_pnts (root_node, y=tree@pd, pnts=pnts)
+             pnts <- get_pnts(root_node, y=tree@pd, pnts=pnts)
              # add 10% to min y limit for node label
              min_y <- abs(min(pnts$y))
              min_y <- min_y + (min_y*.1)
              min_y <- ifelse(min(pnts$y) > 0, min_y, -1*min_y)
-             y_lmts <- c (min_y, max(pnts$y))
-             plot.default (x=pnts$x, y=pnts$y, col='black', pch=19, yaxt='n', ylab='',
+             y_lmts <- c(min_y, max(pnts$y))
+             plot.default(x=pnts$x, y=pnts$y, col='black', pch=19, yaxt='n', ylab='',
                            xlab='', bty='n', ylim=y_lmts)
              if(taxonyms) {
-               text (x=pnts$x, y=pnts$y,
+               text(x=pnts$x, y=pnts$y,
                      labels=sapply(pnts$node, function(n) tree@nodelist[[n]][['taxonym']]),
                      pos=1)
              } else {
-               text (x=pnts$x, y=pnts$y, labels=pnts$node, pos=1)
+               text(x=pnts$x, y=pnts$y, labels=pnts$node, pos=1)
              }
              # draw lines
-             for (i in 2:nrow (pnts)) {
+             for(i in 2:nrow (pnts)) {
                prenode <- tree@nodelist[[pnts$node[i]]][['prid']]
-               ind <- c (i, which (pnts$node == prenode))
-               lines (x=pnts$x[ind], y=pnts$y[ind])
+               ind <- c(i, which(pnts$node == prenode))
+               lines(x=pnts$x[ind], y=pnts$y[ind])
              }
            })
