6a3924f2cac08d765057a88eb9e91016a34a7e05
Dominic Bennett <dominic.john.bennett@gmail.com>
2016-02-10 15:13:14 +0000

    Updated compare

--- other/compare.R
+++ other/compare.R
@@ -4,6 +4,48 @@
 # LIBS
 library(ggplot2)
 
+addTip <- function(tree, id, sister, start, end,
+                   parent_id=paste0("p_", id),
+                   tip_taxonym=NULL, parent_taxonym=NULL) {
+  updatePre <- function(node) {
+    node[['kids']] <- c(node[['kids']], tip[['id']])
+    node[['pd']] <- node[['pd']] + tip[['span']]
+    node
+  }
+  tip <- list('id'=id)
+  if(!is.null(tip_taxonym)) {
+    tip[['taxonym']] <- tip_taxonym
+  }
+  node <- list('id'=parent_id)
+  if(!is.null(parent_taxonym)) {
+    node[['taxonym']] <- parent_taxonym
+  }
+  tip[['span']] <- start - end
+  age <- getNodeAge(tree, sister)
+  new_sister <- sister <- tree@nodelist[[sister]]
+  new_parent <- tree@nodelist[[sister[['prid']]]]
+  new_parent[['ptid']] <- new_parent[['ptid']][!new_parent[['ptid']] %in% sister[['id']]]
+  new_parent[['ptid']] <- c(new_parent[['ptid']], node[['id']])
+  new_sister[['span']] <- start - age
+  new_sister[['prid']] <- node[['id']]
+  node[['span']] <- sister[['span']] - new_sister[['span']]
+  node[['pd']] <- new_sister[['span']] + tip[['span']]
+  node[['prdst']] <- sister[['prdst']] - new_sister[['span']]
+  node[['prid']] <- sister[['prid']]
+  node[['ptid']] <- node[['children']] <- c(tip[['id']], sister[['id']])
+  tip[['pd']] <- 0
+  tip[['prdst']] <- node[['prdst']] + tip[['span']]
+  tip[['prid']] <- node[['id']]
+  tree@nodelist[[tip[['id']]]] <- tip
+  tree@nodelist[[node[['id']]]] <- node
+  tree@nodelist[[new_sister[['id']]]] <- new_sister
+  tree@nodelist[[new_parent[['id']]]] <- new_parent
+  pres <- getNodePrid(tree, node[['id']])
+  tree@nodelist[pres] <- lapply(tree@nodelist[pres],
+                                updatePre)
+  treeman:::.updateSlots(tree)
+}
+
 phyloBuilder <- function (start_tree, max_size, sample_size) {
   require(MoreTreeTools)
   tree <- read.tree(text=start_tree)
@@ -46,15 +88,16 @@ treemanBuilder <- function (start_tree, max_size, sample_size) {
 
 # RUN
 start_tree <- "(t1:1.0,t2:1.0);"
-max_size <- 500  # maximum size of tree
+max_size <- 1000  # maximum size of tree
 sample_size <- 10
-phylo_res <- phyloBuilder(start_tree, max_size, sample_size)
 treeman_res <- treemanBuilder(start_tree, max_size, sample_size)
+phylo_res <- phyloBuilder(start_tree, max_size, sample_size)
 
 # PLOT
 res <- data.frame("Tree_size"=seq(2, max_size, sample_size),
                   "Timing"=c(phylo_res, treeman_res),
                   "Build_method"=rep(c('Phylo', "TreeMan"), each=max_size/sample_size))
-save(res, file=file.path(getwd(), "compare_results.Rd"))
+#save(res, file=file.path('other', "compare.Rd"))
 p <- ggplot (res, aes(y=Timing, x=Tree_size, colour=Build_method)) + geom_line(size=4)
-p + theme_bw() + theme(text=element_text(size=20))
+p + theme_bw() + theme(text=element_text(size=20)) + ylab(label = "Timing(s)") +
+  xlab(label = "N. tips")
