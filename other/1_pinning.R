9f7563a0ce8b2a26b65f782ea4624a796bc811a9
Dominic Bennett <dominic.john.bennett@gmail.com>
2016-02-11 17:09:35 +0000

    Created

--- other/1_pinning.R
+++ other/1_pinning.R
@@ -1,1 +1,20 @@
-# take part of the mammalian tree and pin missing tips to it using taxonize
+# Pinning mammal species to mammalian supertree using taxonomy
+
+# LIBS
+library(treemantools)  # packge with online resolution functions
+
+# DATA
+data(mammals)
+data(rslvd_mammals)  # rslvd names (i.e. with lineages) not in mammals tree
+
+# PARAMETERS
+n <- 100  # number of missing mammal species to pin
+
+# PIN
+rnds <- sample(1:nrow(rslvd_mammals), n)
+rslvd_mammals <- rslvd_mammals[rnds, ]
+lngs <- mlply(rslvd_mammals, function(lineage, ...) strsplit(lineage, '\\|')[[1]])
+ids <- gsub("\\s+", "_", rslvd_mammals$search_name)
+ends <- rep(0, length(ids))  # all tips end in the present
+pinned_tree <- pinTips(tree=mammals, lngs=lngs, tids=ids, ends=ends)
+ids %in% pinned_tree['tips']
