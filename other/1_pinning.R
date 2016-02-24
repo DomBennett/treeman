e0b857106ea75435a7ab16ef68e9dbb49fb6e440
Dominic Bennett <dominic.john.bennett@gmail.com>
2016-02-24 20:03:29 +0000

    Minor changes

--- other/1_pinning.R
+++ other/1_pinning.R
@@ -17,10 +17,10 @@ n <- 100  # number of missing mammal species to pin
 rnds <- sample(1:nrow(rslvd_mammals), n)
 rslvd_mammals <- rslvd_mammals[rnds, ]
 lngs <- mlply(rslvd_mammals, function(lineage, ...) strsplit(lineage, '\\|')[[1]])
-ids <- gsub("\\s+", "_", rslvd_mammals$search_name)  # always replace spaces with _
-ends <- rep(0, length(ids))  # all tips end in the present
-pinned_tree <- pinTips(tree=mammals, lngs=lngs, tids=ids, ends=ends)
-p_added <- sum(ids %in% pinned_tree['tips'])*100/n
+tids <- gsub("\\s+", "_", rslvd_mammals$search_name)  # always replace spaces with _
+ends <- rep(0, length(tids))  # all tips end in the present
+pinned_tree <- pinTips(tree=mammals, lngs=lngs, tids=tids, ends=ends)
+p_added <- sum(tids %in% pinned_tree['tips'])*100/n
 cat('[', p_added, '%] of n pinned to mammals\n', sep='')
 
 # VIZ
@@ -33,11 +33,11 @@ writeTree(pinned_tree, file='temp.tre', nodeLabels=txnym)
 tree_phylo <- ape::read.tree('temp.tre')
 plot(tree_phylo, show.tip.label=FALSE, edge.width=0.5, type='fan', no.margin=FALSE,
      edge.color='lightsteelblue3')
-ids <- ids[ids %in% tree_phylo$tip.label]
-sids <- sample(ids, 10, prob=1/sapply(ids, nchar))  # only plot 10, easier to see
-indx <- match(sids, tree_phylo$tip.label)
+tids <- tids[tids %in% tree_phylo$tip.label]
+print_names <- sample(tids, 10, prob=1/sapply(tids, nchar))  # only plot 10, easier to see
+indx <- match(print_names, tree_phylo$tip.label)
 ape::tiplabels(pch=19, tip=indx, cex=0.9, col='black')
-ape::tiplabels(text=gsub('_', ' ', sids),
+ape::tiplabels(text=gsub('_', ' ', print_names),
                tip=indx, cex=0.85, adj=c(-0.05, -0.25),
                bg=NULL, col='black', frame='none', font=3)
 # example large clades for guidance
