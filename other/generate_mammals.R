78ec30663c66d7c24ff56b115706dab465373b64
Dominic Bennett <dominic.john.bennett@gmail.com>
2016-04-16 12:05:19 +0100

    Updated given freeman overhaul

--- other/generate_mammals.R
+++ other/generate_mammals.R
@@ -18,11 +18,11 @@ load("other/mammalia_resolvedlist.Rd")
   subj[length (subj)]
 }
 
-for(i in 1:length(mammals@nodelist)) {
-  print(mammals@nodelist[[i]][['id']])
+for(i in 1:length(mammals@ndlst)) {
+  print(mammals@ndlst[[i]][['id']])
   done <- FALSE
-  if(!is.null(mammals@nodelist[[i]][['kids']])) {
-    trms <- mammals@nodelist[[i]][['kids']]
+  if(!is.null(mammals@ndlst[[i]][['kids']])) {
+    trms <- mammals@ndlst[[i]][['kids']]
     trms <- sub("_", " ", trms)
     indxs <- resolve.list$resolved$search.name %in% trms
     if(sum(indxs) > 1) {
@@ -30,24 +30,24 @@ for(i in 1:length(mammals@nodelist)) {
       bool <- unlist(lapply(lngs, function(l) 'Mammalia' %in% l))
       lngs <- lngs[bool]
       if(length(lngs) > 1) {
-        mammals@nodelist[[i]][['txnym']] <- .findClade(lngs)
+        mammals@ndlst[[i]][['txnym']] <- .findClade(lngs)
         done <- TRUE
       }
       if(length(lngs) == 1) {
         lng <- resolve.list[['lineages']][indxs][[1]]
-        mammals@nodelist[[i]][['txnym']] <- lng[length(lng)-1]
+        mammals@ndlst[[i]][['txnym']] <- lng[length(lng)-1]
         done <- TRUE
       }
     }
     if(!done) {
-      mammals@nodelist[[i]][['txnym']] <- 'Unknown'
+      mammals@ndlst[[i]][['txnym']] <- 'Unknown'
     }
   } else {
-    mammals@nodelist[[i]][['txnym']] <- strsplit(mammals@nodelist[[i]][['id']], "_")[[1]]
+    mammals@ndlst[[i]][['txnym']] <- strsplit(mammals@ndlst[[i]][['id']], "_")[[1]]
   }
 }
 
-mammals[['Homo_sapiens']]
+mammals@ndlst[['Homo_sapiens']]
 mammals[['n816']]
 mammals[['n815']]
 mammals[['n814']]
@@ -66,7 +66,7 @@ mammals[['n4']]
 mammals[['n1']]
 
 getTxnym <- function(tree, txnym) {
-  # get node id(s) for a taxonym
+  # get nd id(s) for a taxonym
   .get <- function(id, txnym, ...) {
     if(rfrnc_txnym %in% txnym) {
       ids <<- c(id, ids)
@@ -74,12 +74,12 @@ getTxnym <- function(tree, txnym) {
   }
   rfrnc_txnym <- txnym
   ids <- NULL
-  m_ply(tree@nodelist, .fun=.get)
+  m_ply(tree@ndlst, .fun=.get)
   ids
 }
 
 getTxnyms <- function(tree, txnyms) {
-  # get node id(s) for taxonyms
+  # get nd id(s) for taxonyms
   .get <- function(id, txnym, ...) {
     for(t in txnyms) {
       if(t %in% txnym) {
@@ -88,7 +88,7 @@ getTxnyms <- function(tree, txnyms) {
     }
   }
   res <- list()
-  m_ply(tree@nodelist, .fun=.get)
+  m_ply(tree@ndlst, .fun=.get)
   res
 }
 
@@ -98,4 +98,4 @@ mammalian_orders <- c('Macroscelidea', 'Afrosoricida', 'Tubulidentata', 'Hyracoi
                       'Cetacea', 'Artiodactyla', 'Chiroptera', 'Perissodactyla', 'Carnivora',
                       'Pholidota')
 
-save(mammals, file="mammals.Rd")
+save(mammals, file="data/mammals.rda", compress="xz")
