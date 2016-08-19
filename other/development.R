feded2b5ad6229b1d9c77f33a385df8af2a75752
Dominic Bennett <dominic.john.bennett@gmail.com>
2016-08-19 18:08:54 +0100

    Outline for version 2

--- other/development.R
+++ other/development.R
@@ -1,19 +1,6 @@
-# TODO:
-# -- read/write newick
-library(treeman)
-
-tree <- readTree("~/Desktop/bininda.tre")
-
-
-tips(tree)
-age(tree)
-calcFairProp(tree, sample(tips(tree), 10))
-calcDstMtrx(tree)
-
-tree[['n1']]
-tree[['n5']]
-tree[['n4']]
-
-trstr <- "((A:1.0,B:1.0):1.0,(C:1.0,(E:1.0,D:1.0):1.0):1.0);"
-tree <- readTree(tree_string=trstr)
-viz(tree)
+# Version 2 aims
+# explore use of ndmtrx and bigmemory
+# get bigmemory at c level http://gallery.rcpp.org/articles/using-bigmemory-with-rcpp/
+# re-write all c code for cpp and big.matrix
+# remove ndlst?
+# main obstacle: getting prid order (run it from looping over pinds)
